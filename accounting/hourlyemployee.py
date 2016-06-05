16f7e1cc73528f05a1c16b3bd9d2deac9b6789d9
benzlt21 <bltaylor.pr1@gmail.com>
2016-06-05 19:29:14 +0000

    Add files via upload

--- accounting/hourlyemployee.py
+++ accounting/hourlyemployee.py
@@ -4,7 +4,7 @@ from accounting.employee import Employee
 class HourlyEmployee(Employee):
 
     def __init__(self, employee_id, first_name, last_name, hourly_rate, weekly_dues):
-        Employee.__init__(first_name, last_name, weekly_dues)
+        Employee.__init__(employee_id, first_name, last_name, weekly_dues)
         self.__hourly_rate = hourly_rate
 
     def get_hourly_rate(self):
